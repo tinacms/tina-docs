b96cbcb69e1f82cf619aaa193f4f921bf01826ad
joshbermanssw <joshberman@ssw.com.au>
2025-02-21 17:10:10 +1100

    added run-time theme updater

--- components/SiteLayout.tsx
+++ components/SiteLayout.tsx
@@ -1,13 +1,31 @@
+// SiteLayout.tsx (server component)
 import React from "react";
+import ThemeUpdater from "./ThemeProvider";
+import getGlobalSiteConfig from "../utils/getGlobalSiteConfig";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-export const SiteLayout = ({ children }: LayoutProps) => {
+export const SiteLayout = async ({ children }: LayoutProps) => {
+  const globalConfig = await getGlobalSiteConfig();
+
+  const useCustomBackground = !globalConfig?.customColorToggle?.disableColor;
+
+  const backgroundClass = useCustomBackground
+    ? "bg-[var(--backgroundColor)]"
+    : "bg-blob-bg";
+
+
   return (
-    <div className="flex flex-col min-h-screen blob-bg font-sans bg-blob-bg bg-[length:100%_100%] bg-top bg-fixed">
-      <div className="flex flex-col flex-1">{children}</div>
-    </div>
+    <>
+      
+      <ThemeUpdater />
+      <div
+        className={`flex flex-col min-h-screen font-sans ${backgroundClass} bg-[length:100%_100%] bg-top bg-fixed`}
+      >
+        <div className="flex flex-col flex-1">{children}</div>
+      </div>
+    </>
   );
 };
