7544648c47f19431ca36c377018914f19977ab32
joshbermanssw <joshberman@ssw.com.au>
2025-02-21 15:27:53 +1100

    add color config

--- tina/collections/siteConfig.tsx
+++ tina/collections/siteConfig.tsx
@@ -20,6 +20,43 @@ export const globalSiteConfiguration = {
             label: "Documentation Site Title",
             type: "string",
         },
-
+        {
+            name: 'siteColors',
+            label: 'Site Colors',
+            type: 'object',
+            fields: [
+                {
+                    name: 'primaryStart',
+                    label: 'Primary Color | Gradient Start',
+                    type: 'string',
+                    description: 'This is the start of the primary color gradient ⚠️ If you want a solid color leave the end and via empty ⚠️',
+                    ui: {
+                        component: 'color',
+                        colorFormat: 'hex',
+                        widget: 'sketch',
+                      },
+                },
+                {
+                    name: 'primaryEnd',
+                    label: 'Primary Color | Gradient End',
+                    type: 'string',
+                    ui: {
+                        component: 'color',
+                        colorFormat: 'hex',
+                        widget: 'sketch',
+                      },
+                },
+                {
+                    name: 'primaryVia',
+                    label: 'Primary Color | Gradient Via',
+                    type: 'string',
+                    ui: {
+                        component: 'color',
+                        colorFormat: 'hex',
+                        widget: 'sketch',
+                      },
+                }
+            ]
+        }
     ]
 }
