f2e046f51f28ab4040f43244d6702f9a5cadbc1e
joshbermanssw <joshberman@ssw.com.au>
2025-02-26 09:28:42 +1100

    add revalidateo n globalSiteConfig

--- utils/getGlobalSiteConfig.tsx
+++ utils/getGlobalSiteConfig.tsx
@@ -1,6 +1,6 @@
 import client from "../tina/__generated__/client";
 
 export default async function getGlobalSiteConfig(){
-    const globalSiteConfig = await client.queries.globalSiteConfiguration( { relativePath: 'global-config.md'});
+    const globalSiteConfig = await client.queries.globalSiteConfiguration( { relativePath: 'global-config.md'}, {fetchOptions: {next: {revalidate: 10}}});
     return globalSiteConfig.data.globalSiteConfiguration;
 }
