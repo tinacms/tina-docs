ac93ec1ce3b4aeb98b42578a1fff945465dcc998
joshbermanssw <joshberman@ssw.com.au>
2025-03-26 17:21:57 +1100

    import react

--- app/ThemeProvider.tsx
+++ app/ThemeProvider.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import {  useEffect } from "react";
+import React, { useEffect } from "react";
 import { client } from "../tina/__generated__/client";
 
 type Props = {
   children: React.ReactNode;
 };
 
-
 export const ThemeProvider = ({ children }: Props) => {
-
-
   useEffect(() => {
     const fetchTheme = async () => {
       const data = await client.queries.GlobalSiteColours({
