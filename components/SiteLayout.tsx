f48462eb7bfbd45c0b960057203715b26c343949
joshbermanssw <joshberman@ssw.com.au>
2025-03-26 11:59:23 +1100

    fix sitelayout

--- components/SiteLayout.tsx
+++ components/SiteLayout.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-export const SiteLayout = ({ children }: LayoutProps) => {
+export const SiteLayout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen blob-bg font-sans bg-blob-bg bg-[length:100%_100%] bg-top bg-fixed">
       <div className="flex flex-col flex-1">{children}</div>
