ddcd1a7b3336c74af1fac3426a45de4ca0fc54e2
joshbermanssw <joshberman@ssw.com.au>
2025-03-26 17:28:26 +1100

    make types conditional

--- components/SiteLayout.tsx
+++ components/SiteLayout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 interface LayoutProps {
-  children: React.ReactNode;
+  children?: React.ReactNode;
 }
 
 export const SiteLayout: React.FC<LayoutProps> = ({ children }) => {
