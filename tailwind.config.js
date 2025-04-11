a715093f32a1984bd0427cccaac5252d9544a5e2
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-04-11 10:56:07 +1000

    fixing the issue with tailwind compatibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/styles/**/*.css",
   ],
   theme: {
     extend: {
