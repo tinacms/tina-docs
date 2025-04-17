8917719f0d4265cd0b34bed317f4ac7c9f509828
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-04-17 15:15:05 +1000

    Adding header component for readability

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -4,50 +4,45 @@ import { NavigationSideBar } from "@/src/components/docs/NavigationSideBar";
 import { getDocsNav } from "@/utils/docs/getDocumentNavigation";
 import type React from "react";
 
+const NAVIGATION_SIDE_BAR_TITLE = "TinaDocs";
+
 export default async function DocsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   // Fetch navigation data that will be shared across all docs pages
-  const NavigationDocsData = await getDocsNav();
-  const headerComponent = (
-    <div className="pl-6">
-      <h1
-        className={
-          "bg-gradient-to-br from-orange-400 via-orange-500 to-orange-600 bg-clip-text pb-4 pt-6 font-tuner text-4xl text-transparent"
-        }
-      >
-        TinaDocs
-      </h1>
-    </div>
-  );
+  const navigationDocsData = await getDocsNav();
 
   return (
     <div className="relative my-6 flex items-start justify-center lg:mb-16 xl:mt-16">
-      <div
-        className={
-          "grid w-full max-w-[2000px] grid-cols-1 px-3 md:grid-cols-[1.25fr_3fr] md:px-8 xl:grid-cols-[1.25fr_3fr_0.75fr] xl:px-16"
-        }
-      >
-        <div className={"sticky top-32 hidden h-[calc(100vh)] md:block"}>
+      <div className="grid w-full max-w-[2000px] grid-cols-1 px-3 md:grid-cols-[1.25fr_3fr] md:px-8 xl:grid-cols-[1.25fr_3fr_0.75fr] xl:px-16">
+        <aside className={"sticky top-32 hidden h-[calc(100vh)] md:block"}>
           <NavigationSideBar
-            tableOfContents={NavigationDocsData.data}
-            header={headerComponent}
+            tableOfContents={navigationDocsData.data}
+            header={<HeaderComponent />}
           />
-        </div>
+        </aside>
         {/* CONTENT COLUMN */}
-        <div className="col-span-2 mx-8 px-2 md:col-span-1 xl:col-span-2">
+        <main className="col-span-2 mx-8 px-2 md:col-span-1 xl:col-span-2">
           <div className="block md:hidden">
             <div className="relative">
-              <DirectoryOverflowButton tocData={NavigationDocsData.data} />
+              <DirectoryOverflowButton tocData={navigationDocsData.data} />
             </div>
           </div>
 
-          <Breadcrumbs navItems={NavigationDocsData.data} />
+          <Breadcrumbs navItems={navigationDocsData.data} />
           {children}
-        </div>
+        </main>
       </div>
     </div>
   );
 }
+
+const HeaderComponent = () => (
+  <div className="pl-6">
+    <h1 className="bg-gradient-to-br from-orange-400 via-orange-500 to-orange-600 bg-clip-text pb-4 pt-6 font-tuner text-4xl text-transparent">
+      {NAVIGATION_SIDE_BAR_TITLE}
+    </h1>
+  </div>
+);
