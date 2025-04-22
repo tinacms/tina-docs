b3df1e6573d63f49820e3ee2e5d32e85b5d7929c
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-04-22 16:17:33 +1000

    fixing lint issues with formatting

--- src/components/navigation/navigation-items.tsx
+++ src/components/navigation/navigation-items.tsx
@@ -117,8 +117,8 @@ const NavTitle: React.FC<NavTitleProps> = ({
   const selectedClass = selected
     ? "selected"
     : childSelected
-    ? "childSelected"
-    : "default";
+      ? "childSelected"
+      : "default";
   const classes =
     level < 1
       ? headerLevelClasses[headerLevel]
