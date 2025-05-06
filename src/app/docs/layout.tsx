d65b80c906823f54b4f3a66ea56e160a19e719e4
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-05-06 15:58:01 +1000

    Fixing navbar layout shift when no scroll

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -16,7 +16,11 @@ export default async function DocsLayout({
     <div className="relative my-6 flex items-start justify-center lg:mb-16 xl:mt-16">
       <div className="grid w-full max-w-[2000px] grid-cols-1 px-3 md:grid-cols-[1.25fr_3fr] md:px-8 xl:grid-cols-[1.25fr_3fr_0.75fr] xl:px-16">
         {/* DESKTOP NAVIGATION SIDEBAR */}
-        <aside className={"sticky top-32 hidden md:block h-[calc(100vh-8rem)]"}>
+        <aside
+          className={
+            "sticky top-32 hidden md:block my-16 h-[calc(100vh-16rem)]"
+          }
+        >
           <NavigationSideBar tableOfContents={navigationDocsData.data} />
         </aside>
         {/* CONTENT COLUMN */}
