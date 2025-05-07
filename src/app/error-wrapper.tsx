4e908f4683618ecc7d12dfd80c2b899af310359e
joshbermanssw <joshberman@ssw.com.au>
2025-05-07 12:36:26 +1000

    rm comments

--- src/app/error-wrapper.tsx
+++ src/app/error-wrapper.tsx
@@ -33,9 +33,6 @@ const ErrorWrapper = ({
                   <a
                     key={link.linkUrl}
                     href={link.linkUrl}
-                    // passHref
-                    // key={link.linkUrl}
-
                     className="text-slate-500 shadow-sm hover:shadow-md outline outline-slate-200 hover:text-slate-700 rounded-md p-2 bg-white/50 hover:bg-white/90"
                   >
                     <div>{link.linkText ?? "External Link 🔗"}</div>
