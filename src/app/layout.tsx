1eb035f9d293b309695b523e5ee8fd07418a718a
joshbermanssw <joshberman@ssw.com.au>
2025-05-09 14:54:55 +1000

    theme PoC

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,28 @@
-import type React from "react";
-import "@/styles/global.css";
-import AdminLink from "@/src/components/ui/admin-link";
-import { TailwindIndicator } from "../components/ui/tailwind-indicator";
+import '@/styles/global.css'
+import AdminLink from '@/src/components/ui/admin-link'
+import { TailwindIndicator } from '@/components/ui/tailwind-indicator'
+
+function getThemeFromFile() {
+  try {
+    const fs = require('fs')
+    const path = require('path')
+    const file = fs?.readFileSync(path.join(process.cwd(), '.theme.json'), 'utf-8')
+    return JSON.parse(file).theme || 'default'
+  } catch {
+    return 'default'
+  }
+}
 
 export default function RootLayout({
-  children = null, //default value for cms
+  children = null,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
+  const theme = getThemeFromFile()
+  console.log(theme);
+
   return (
-    <html lang="en">
+    <html lang="en" className={`theme-${theme} dark`}>
       <head>
         <meta name="theme-color" content="#E6FAF8" />
         <link rel="alternate" type="application/rss+xml" href="/rss.xml" />
@@ -22,5 +35,5 @@ export default function RootLayout({
         </div>
       </body>
     </html>
-  );
+  )
 }
