a903e9e551eaabe88c028dcf3d7f8d02b865f5ab
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-05-09 15:55:56 +1000

    testing extra config with static build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
+const isStatic = process.env.EXPORT_MODE === "static";
+
 /** @type {import('next').NextConfig} */
+// biome-ignore lint/style/useConst: <explanation>
+let extraConfig = {};
+
+if (isStatic) {
+  extraConfig.output = "export";
+  extraConfig.images = {
+    unoptimized: true,
+  };
+}
 const nextConfig = {
-  output: "export",
+  ...extraConfig,
   async rewrites() {
     return [
       {
