fb1bbdd4304025b2d34c71b7e02ab37ee75e7af9
joshbermanssw <joshberman@ssw.com.au>
2025-05-12 09:45:56 +1000

    Implement Shiki

---
+++ src/components/tina-markdown/standard-elements/code-block/code-block.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import { createHighlighter } from "shiki";
+import "./code-block.css";
+import {
+  transformerNotationDiff,
+  transformerNotationFocus,
+  transformerNotationHighlight,
+} from "@shikijs/transformers";
+import { FaCheck } from "react-icons/fa";
+export function CodeBlock({
+  value,
+  lang = "ts",
+  showCopyButton = true,
+}: {
+  value: string;
+  lang?: string;
+  showCopyButton?: boolean;
+}) {
+  const [html, setHtml] = useState("");
+  const [isCopied, setIsCopied] = useState(false);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const load = async () => {
+      const highlighter = await createHighlighter({
+        themes: ["night-owl"],
+        langs: [lang],
+      });
+
+      const code = await highlighter.codeToHtml(value, {
+        lang,
+        theme: "night-owl",
+        transformers: [
+          transformerNotationDiff({ matchAlgorithm: "v3" }),
+          transformerNotationHighlight({ matchAlgorithm: "v3" }),
+          transformerNotationFocus({ matchAlgorithm: "v3" }),
+        ],
+        meta: {
+          showLineNumbers: true,
+        },
+      });
+
+      if (isMounted) setHtml(code);
+    };
+
+    load();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [value, lang]);
+
+  console.log(lang);
+
+  return (
+    <div className="relative w-full my-2 group">
+      <div className={`absolute top-0 right-0 z-10 px-2 py-1 text-xs font-mono text-[#d6deeb] transition-opacity duration-200 opacity-100 group-hover:opacity-0 group-hover:pointer-events-none ${showCopyButton ? "opacity-100" : "opacity-0"}`}>
+        {lang}
+      </div>
+      <div className="absolute top-0 right-0 z-10 mx-2 my-1 text-xs font-mono bg-[#222] transition-opacity duration-200 opacity-0 group-hover:opacity-100 cursor-pointer">
+        <button
+          onClick={() => {
+            navigator.clipboard.writeText(value);
+            setIsCopied(true);
+            setTimeout(() => setIsCopied(false), 1000);
+          }}
+          className="px-2 py-1 text-[#d6deeb] rounded transition cursor-pointer flex items-center gap-1"
+        >
+          {isCopied ? <FaCheck size={12} /> : 'Copy'}
+        </button>
+      </div>
+      <div
+        className="shiki w-full overflow-x-auto rounded-lg bg-[#011627] p-4 text-sm"
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
+    </div>
+  );
+  
+}
