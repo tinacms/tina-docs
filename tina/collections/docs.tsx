32c28fb81f192c7d4628d13407e3e79a1994088d
joshbermanssw <joshberman@ssw.com.au>
2025-05-12 16:46:40 +1000

    api reference schema

--- tina/collections/docs.tsx
+++ tina/collections/docs.tsx
@@ -1,5 +1,5 @@
 import AccordionTemplate, { AccordionBlockTemplate } from "@/tina/templates/markdown-embeds/accordion.template";
-import ApiReferenceTemplate from "@/tina/templates/markdown-embeds/type-reference.template";
+import TypeReferenceTemplate from "@/tina/templates/markdown-embeds/type-reference.template";
 import CalloutTemplate from "@/tina/templates/markdown-embeds/callout.template";
 import CardGridTemplate from "@/tina/templates/markdown-embeds/card-grid.template";
 import QueryResponseTabsTemplate from "@/tina/templates/markdown-embeds/query-response-tabs.template";
@@ -9,6 +9,7 @@ import YoutubeTemplate from "@/tina/templates/markdown-embeds/youtube.template";
 import type { Template } from "tinacms";
 import SeoInformation from "./seo-information";
 import { StepsTemplate } from "../templates/markdown-embeds/steps.template";
+import { ApiReferenceTemplate } from "../templates/markdown-embeds/api-reference.template";
 
 export const docsCollection = {
   name: "docs",
@@ -92,11 +93,12 @@ export const docsCollection = {
         RecipeTemplate as Template,
         AccordionTemplate as Template,
         AccordionBlockTemplate as Template,
-        ApiReferenceTemplate as Template,
+        TypeReferenceTemplate as Template,
         YoutubeTemplate as Template,
         QueryResponseTabsTemplate as Template,
         CalloutTemplate as Template,
         StepsTemplate as Template,
+        ApiReferenceTemplate as Template,
       ],
     },
   ],
---
+++ tina/templates/markdown-embeds/api-reference.template.tsx
@@ -0,0 +1,102 @@
+export const ApiReferenceTemplate = {
+  name: "apiReference",
+  label: "API Reference",
+  fields: [
+    {
+      name: "endpointName",
+      label: "Endpoint Name",
+      type: "string",
+      description: "Such like /content/:collection",
+    },
+    {
+      name: "description",
+      label: "Description",
+      type: "string",
+    },
+    {
+      name: "type",
+      label: "Type",
+      type: "string",
+      options: ["GET", "POST", "PUT", "DELETE"],
+    },
+    {
+      name: "pathParameters",
+      label: "Path Parameters",
+      list: true,
+      type: "object",
+      fields: [
+        { name: "parameterName", label: "Parameter Name", type: "string" },
+        {
+          name: "parameterDescription",
+          label: "Parameter Description",
+          type: "string",
+          ui: {
+            component: "textarea",
+          },
+        },
+        {
+          name: "required",
+          label: "Required",
+          type: "boolean",
+        },
+        {
+          name: "in",
+          label: "In",
+          type: "string",
+          options: ["path", "query", "body"],
+        },
+        {
+          name: "type",
+          label: "Type",
+          type: "string",
+          options: ["string", "number", "boolean", "integer"],
+        },
+      ],
+    },
+    {
+      name: "responses",
+      label: "Responses",
+      list: true,
+      type: "object",
+      fields: [
+        { name: "status", label: "Status Code", type: "number" },
+        { name: "description", label: "Description", type: "string" },
+        {
+            name: "responseBody",
+            label: "Response Body Schema",
+            type: "object",
+            list: true,
+            fields: [
+              { name: "fieldName", label: "Field Name", type: "string" },
+              {
+                name: "type",
+                label: "Type",
+                type: "string",
+                options: ["string", "number", "integer", "boolean", "array", "object"]
+              },
+              { name: "description", label: "Description", type: "string" },
+              {
+                name: "fields",
+                label: "Nested Fields (if type is object)",
+                type: "object",
+                list: true,
+                fields: [
+                  { name: "fieldName", label: "Field Name", type: "string" },
+                  {
+                    name: "type",
+                    label: "Type",
+                    type: "string",
+                    options: ["string", "number", "integer", "boolean", "array", "object"]
+                  },
+                  { name: "description", label: "Description", type: "string" }
+                ],
+                ui: {
+                  condition: (item) => item.type === "object"
+                }
+              }
+            ]
+          }
+      ],
+    },
+  ],
+};
