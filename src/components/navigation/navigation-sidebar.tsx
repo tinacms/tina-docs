05afb382c52bb2358f13bd49b2b6a8a1c20c11fc
isaaclombardssw <isaaclombard@ssw.com.au>
2025-05-27 16:24:05 +1000

    menu heigh

--- src/components/navigation/navigation-sidebar.tsx
+++ src/components/navigation/navigation-sidebar.tsx
@@ -10,7 +10,7 @@ export const NavigationSideBar = ({
 }) => {
   return (
     <div className="w-full h-full rounded-2xl brand-glass-gradient dark:border dark:border-neutral-border-subtle/60 shadow-xl">
-      <div className="h-[calc(100%-230px)] overflow-y-auto overflow-x-hidden pl-4 2xl:pl-0">
+      <div className="h-[calc(100vh-2rem)] overflow-y-auto overflow-x-hidden pl-4 2xl:pl-0">
         <DocsNavigationItems navItems={tableOfContents.items} />
       </div>
     </div>
