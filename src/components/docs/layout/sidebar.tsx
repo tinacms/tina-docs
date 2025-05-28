3924aa020f58f11cdb74f0a9e56a6d01fcb2f60b
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-05-28 14:17:53 +1000

    Updating Searching Behaviour onClick  (#67)
    * Search functionality - Closing search result on click outside
    
    * fixing search term
    
    * cleaning the search term
    
    * fixing styling for navbar
    
    * fixing horizontal padding for mobile view
    
    * fixing linting

--- src/components/docs/layout/sidebar.tsx
+++ src/components/docs/layout/sidebar.tsx
@@ -10,7 +10,7 @@ export const Sidebar = ({
     <>
       {tabs.map((tab) => (
         <Tabs.Content key={tab.label} value={tab.label}>
-          <aside className="sticky top-4 hidden md:block mr-4 h-[calc(100vh-2rem)] xl:w-84 justify-self-center w-72 ml-8">
+          <aside className="sticky top-4 hidden lg:block mr-4 h-[calc(100vh-2rem)] xl:w-84 justify-self-center w-72 ml-8">
             <NavigationSideBar
               title={tab?.label}
               tableOfContents={tab?.content}
