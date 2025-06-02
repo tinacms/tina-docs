a769f4b91eda7b4ff256c743193901ac910edb54
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-06-02 13:28:58 +1000

    rearranging the classes for bg div

--- src/components/docs/layout/sidebar.tsx
+++ src/components/docs/layout/sidebar.tsx
@@ -26,7 +26,7 @@ export const Sidebar = ({
   }, []);
 
   return (
-    <div className="sticky hidden lg:block mr-4 h-[calc(100vh-8rem)] w-84 ml-8 top-4 rounded-2xl dark:border dark:border-neutral-border-subtle/60 shadow-xl brand-glass-gradient">
+    <div className="sticky hidden brand-glass-gradient lg:block mr-4 h-[calc(100vh-8rem)] w-84 ml-8 top-4 rounded-2xl dark:border dark:border-neutral-border-subtle/60 shadow-xl">
       <div className="relative w-full h-full overflow-hidden">
         <div
           className="absolute inset-0 flex transition-transform duration-300 ease-in-out"
