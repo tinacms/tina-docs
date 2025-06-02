6a5001b7bd713c7ec39b1082acb34b0644240d6a
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-06-02 13:26:59 +1000

    removing unnecessary divs and classes

--- src/components/docs/layout/sidebar.tsx
+++ src/components/docs/layout/sidebar.tsx
@@ -26,8 +26,8 @@ export const Sidebar = ({
   }, []);
 
   return (
-    <div className="sticky hidden lg:block mr-4 h-[calc(100vh-4rem)] w-84 ml-8 top-4 rounded-lg dark:border dark:border-neutral-border-subtle/60 shadow-xl">
-      <div className="relative w-full h-full overflow-hidden rounded-lg">
+    <div className="sticky hidden lg:block mr-4 h-[calc(100vh-8rem)] w-84 ml-8 top-4 rounded-2xl dark:border dark:border-neutral-border-subtle/60 shadow-xl brand-glass-gradient">
+      <div className="relative w-full h-full overflow-hidden">
         <div
           className="absolute inset-0 flex transition-transform duration-300 ease-in-out"
           style={
@@ -37,7 +37,7 @@ export const Sidebar = ({
           }
         >
           {tabs.map((tab) => (
-            <div key={tab.label} className="w-full flex-shrink-0 sticky top-0">
+            <div key={tab.label} className="w-full flex-shrink-0">
               <Tabs.Content value={tab.label}>
                 <NavigationSideBar
                   title={tab?.label}
