f7ea46f97d7ea4cb7efb42133799fe414bb9bb30
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-06-06 12:06:31 +1000

    Adding type for the tab

--- src/components/docs/layout/utils.ts
+++ src/components/docs/layout/utils.ts
@@ -2,7 +2,15 @@ import { hasNestedSlug } from "@/components/navigation/navigation-items/utils";
 
 import { hasMatchingApiEndpoint } from "@/components/navigation/navigation-items/utils";
 
-export const findTabWithPath = (tabs: any[], path: string) => {
+interface Tab {
+  label: string;
+  content: {
+    items: any[];
+    __typename?: string;
+  };
+}
+
+export const findTabWithPath = (tabs: Tab[], path: string) => {
   for (const tab of tabs) {
     if (tab.content?.items && hasNestedSlug(tab.content?.items, path)) {
       return tab.label;
