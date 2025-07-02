a4902939d79c3cd7fb18da0ec491329a8bac401a
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-07-02 15:35:25 +1000

    adding default theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       <body className={`${body.variable} ${heading.variable}`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme={theme}
           enableSystem={true}
           disableTransitionOnChange={false}
         >
