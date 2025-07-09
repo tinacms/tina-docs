e4e6f1aac0356721ec3b8adb17a80a9ef53e0c4f
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-07-10 09:53:22 +1000

    fixing border for the code block

--- src/components/tina-markdown/embedded-elements/recipe.tsx
+++ src/components/tina-markdown/embedded-elements/recipe.tsx
@@ -184,7 +184,7 @@ export const RecipeBlock = (data: {
 
         <div
           ref={codeblockRef}
-          className="flex flex-col top-3 z-10 w-full rounded-b-2xl lg:rounded-r-2xl py-0 bg-neutral-background shadow-sm border border-neutral-border-subtle lg:border-l-0"
+          className="flex flex-col top-3 z-10 w-full rounded-b-2xl lg:rounded-r-2xl py-0 bg-neutral-background shadow-sm border border-neutral-border-subtle lg:border-l-0 lg:rounded-bl-none"
         >
           {code ? (
             <CodeBlockWithHighlightLines
