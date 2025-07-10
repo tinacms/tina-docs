babf4b6884c417cc5a8d28021b8ec5b4c707defb
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-07-10 17:34:25 +1000

    fixing border when no code is added

--- src/components/tina-markdown/embedded-elements/recipe.tsx
+++ src/components/tina-markdown/embedded-elements/recipe.tsx
@@ -296,7 +296,7 @@ export const RecipeBlock = (data: {
 
         <div
           ref={codeblockRef}
-          className="flex flex-col top-3 z-10 h-fit lg:w-[66%] rounded-b-2xl lg:rounded-r-2xl py-0 bg-neutral-background shadow-sm border border-neutral-border-subtle lg:border-l-0"
+          className="flex flex-col top-3 z-10 h-fit lg:w-[66%] rounded-b-2xl lg:rounded-r-2xl py-0 bg-neutral-background shadow-sm border border-neutral-border-subtle lg:border-l-0 lg:rounded-bl-none"
         >
           <div ref={codeContentRef}>
             <div
