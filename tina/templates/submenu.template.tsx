16578a9448971e67fb18c4515de0bca0e7da52cf
Josh Berman [SSW] <JoshBerman@ssw.com.au>
2025-08-08 12:45:35 +1000

    label of levels

--- tina/templates/submenu.template.tsx
+++ tina/templates/submenu.template.tsx
@@ -1,12 +1,12 @@
 import type { Template } from "tinacms";
 import { itemTemplate } from "./navbar-ui.template";
 
-const createMenuTemplate = (templates: Template[]): Template => ({
+const createMenuTemplate = (templates: Template[], level: number): Template => ({
   label: "Submenu",
   name: "items",
   ui: {
     itemProps: (item) => {
-      return { label: `🗂️ ${item?.title ?? "Unnamed Menu Group"}` };
+      return { label: `🗂️ ${level} | ${item?.title ?? "Unnamed Menu Group"}` };
     },
   },
   fields: [
@@ -21,14 +21,14 @@ const createMenuTemplate = (templates: Template[]): Template => ({
   ],
 });
 
-const thirdLevelSubmenu: Template = createMenuTemplate([itemTemplate]);
+const thirdLevelSubmenu: Template = createMenuTemplate([itemTemplate], 3);
 const secondLevelSubmenu: Template = createMenuTemplate([
   thirdLevelSubmenu,
   itemTemplate,
-]);
+], 2);
 export const submenuTemplate: Template = createMenuTemplate([
   secondLevelSubmenu,
   itemTemplate,
-]);
+], 1  );
 
 export default submenuTemplate;
