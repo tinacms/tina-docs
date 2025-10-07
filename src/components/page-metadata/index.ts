d502dbde56757b0e2194609c561e900aa830a2c6
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-10-07 18:00:25 +1100

    fixing linting

--- src/components/page-metadata/index.ts
+++ src/components/page-metadata/index.ts
@@ -1,2 +1,2 @@
-export { default as GitHubMetadata } from './github-metadata';
-export { getPreciseRelativeTime, getRelativeTime } from './timeUtils';
+export { default as GitHubMetadata } from "./github-metadata";
+export { getPreciseRelativeTime, getRelativeTime } from "./timeUtils";
