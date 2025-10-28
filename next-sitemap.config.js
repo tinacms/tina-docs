5ebd7e38ad90f6ad9ae902a0ec8558204f1acb29
Tino Liu [SSW] <tinoliu@ssw.com.au>
2025-10-28 16:38:20 +0800

    1st attempt fix sitemap error (#280)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
+  siteUrl: `${process.env.NEXT_PUBLIC_SITE_URL}${process.env.NEXT_PUBLIC_BASE_PATH || ''}`,
   changefreq: "daily",
   priority: 0.7,
   sitemapSize: 5000,
